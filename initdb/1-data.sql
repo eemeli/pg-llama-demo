bc69157a26aceb72f1a3494b494329f5e8bb3e38
Eemeli Aro <eemeli@gmail.com>
2020-02-26 14:58:34 +0200

    Add herder & llama data

---
+++ initdb/1-data.sql
@@ -0,0 +1,10 @@
+insert into herders (name) values
+  ('Alice'),
+  ('Bob'),
+  ('Chuck');
+
+insert into llamas (name, herder) values
+  ('Mary', 1),
+  ('Missy', 1),
+  ('Nimble', 2),
+  ('Spitter', 3);
